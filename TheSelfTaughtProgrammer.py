091d61f769eee0ab1886c874eabd91b7cf5d2326
Ryan Richardson <ryanrich811@gmail.com>
2023-05-17 12:09:49 -0400

    Chapter 6 exercises Completed

--- TheSelfTaughtProgrammer.py
+++ TheSelfTaughtProgrammer.py
@@ -1,26 +1,58 @@
-# Chapter 5
-favorite_musicians = ["Eminem", "Taylor Swift", "Bob Dylan", "Jay-Z"]
-places_visited = [("40.4173° N", "82.9071° W"), ("27.6648° N", "81.5158° W")]
-Ryan_dict = {"eyes": "brown",
-             "height": "6 foot 4",
-             "weight": 225
-             }
+# Chapter 5 ***********
+# favorite_musicians = ["Eminem", "Taylor Swift", "Bob Dylan", "Jay-Z"]
+# places_visited = [("40.4173° N", "82.9071° W"), ("27.6648° N", "81.5158° W")]
+# Ryan_dict = {"eyes": "brown",
+#              "height": "6 foot 4",
+#              "weight": 225
+#              }
 
-def ask_about_ryan():
-    while True:
-        user_question = input("Type eyes, weight, or height to know my attributes: ")
-        if user_question == "eyes":
-            print(Ryan_dict["eyes"])
-        elif user_question == "height":
-            print(Ryan_dict["height"])
-        elif user_question == "weight":
-            print(Ryan_dict["weight"])
-        else:
-            print("I don't know")
-ask_about_ryan()
+# def ask_about_ryan():
+#     while True:
+#         user_question = input("Type eyes, weight, or height to know my attributes: ")
+#         if user_question == "eyes":
+#             print(Ryan_dict["eyes"])
+#         elif user_question == "height":
+#             print(Ryan_dict["height"])
+#         elif user_question == "weight":
+#             print(Ryan_dict["weight"])
+#         else:
+#             print("I don't know")
+# ask_about_ryan()
 
-musicians_dict = {"Eminem": ["Lose Yourself", "Stan"],
-                  "Taylor Swift": ["Love Story", "Shake it Off"],
-                  "Bob Dylan": ["Like a Rolling Stone", "Blowin' in the Wind"],
-                  "Jay-Z": ["Empire State of Mind", "99 Problems"]
-                 }
+# musicians_dict = {"Eminem": ["Lose Yourself", "Stan"],
+#                   "Taylor Swift": ["Love Story", "Shake it Off"],
+#                   "Bob Dylan": ["Like a Rolling Stone", "Blowin' in the Wind"],
+#                   "Jay-Z": ["Empire State of Mind", "99 Problems"]
+#                  }
+
+# Chapter 6 ***********
+# string = "Camus"
+# for i in string:
+#     print(i)
+
+# user_input_one = input("What did you write yesterday?: ")
+# user_input_two = input("Who did you send it to?: ")
+# print(f"Yesterday I wrote a {user_input_one}. I sent it to {user_input_two}!")
+
+# new_string = "aldos Hoxley was born in 1894"
+# print(new_string.capitalize())
+
+# string = "Where now?, When now?, Who now?"
+# print(string.split(","))
+
+# string = ["The", "fox", "jumped", "over", "the", "fence", "."]
+# print(" ".join(string))
+
+# string = "A screaming comes across the sky"
+# print(string.replace("s","$"))
+
+# dialogue = "It's really just \"—he rubbed his hand over his stubble—\" the most frustrating thing I can think of."
+# print(dialogue)
+
+# con_add = "three " + "three " + "three "
+# con_multiply = "three " * 3
+# print(con_add)
+# print(con_multiply)
+
+# string = "It was a bright cold day in April, and the clocks were striking thirteen."
+# print(string[:33])
