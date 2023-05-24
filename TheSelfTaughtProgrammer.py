19c1cbf8c81b2cd5ef26c23da6bee318cd489800
Ryan Richardson <ryanrich811@gmail.com>
2023-05-24 14:08:52 -0400

    FizzBuzz Chapter 22 Completed

--- TheSelfTaughtProgrammer.py
+++ TheSelfTaughtProgrammer.py
@@ -255,4 +255,18 @@
 # while stack.items:
 #     new_list.append(stack.pop())
 # print(new_list)
+ 
+# Chapter 21 ***********
+def FizzBuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
 
+        else:
+            print(i)
+
+FizzBuzz()
